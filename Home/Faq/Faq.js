e132a296c80b7010306dd681862985282134c40e
bagirovkenan <32015693+bagirovkenan@users.noreply.github.com>
2019-04-21 17:13:27 +0400

    Add files via upload

---
+++ Home/Faq/Faq.js
@@ -0,0 +1,24 @@
+
+$(function () {
+
+    $(document).ready(function () {
+        $(".FaqHiddeColDiv").hide(),
+            $(".ShowFaqtitleFaq").click(function () {
+
+                var HiddendDiv = $(this).parent().children(".FaqHiddeColDiv");
+                var c = $(this).parent().children(".FaqHiddeColDiv").children("p").css("height")
+                var HiddenDivDisplay = $(this).parent().children(".FaqHiddeColDiv").css("display");
+
+                if (HiddenDivDisplay === "none") 
+                {
+                    $(this).addClass("yourClass");
+                    HiddendDiv.show(1000)
+                }
+                else 
+                {
+                    $(this).removeClass("yourClass");
+                    HiddendDiv.hide(1000)
+                }
+            })
+    })
+})
